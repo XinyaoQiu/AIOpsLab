3182b0cf036d777cd88f0eec18cd476c3ed70344
Vincent Han <vhan@bowdoin.edu>
2025-04-06 23:58:33 -0500

    4/6

--- clients/dpo_trainer.py
+++ clients/dpo_trainer.py
@@ -8,4 +8,5 @@ train_dataset = load_dataset("", split="train")
 training_args = DPOConfig(output_dir="Qwen2-0.5B-DPO", logging_steps=10, bf16=True)
 
 trainer = DPOTrainer(model=model, args=training_args, processing_class=tokenizer, train_dataset=train_dataset)
+print("about to train!")
 trainer.train()
