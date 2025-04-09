ff2ac505678627da2a3fc112b8b2d286dc304c01
Vincent Han <vhan@bowdoin.edu>
2025-04-09 17:15:19 -0500

    changed dpo to ppo

--- clients/dpo_trainer.py
+++ clients/dpo_trainer.py
@@ -1,12 +1,24 @@
 from datasets import load_dataset
-from trl import DPOConfig, DPOTrainer
+from trl import PPOConfig, PPOTrainer
 
 model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2-0.5B-Instruct")
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2-0.5B-Instruct")
 train_dataset = load_dataset("", split="train")
 
-training_args = DPOConfig(output_dir="Qwen2-0.5B-DPO", logging_steps=10, bf16=True)
+config = PPOConfig(
+    model_name=model_name,
+    learning_rate=1.41e-5,
+    batch_size=16,
+    log_with=None,  # or "wandb"
+    mini_batch_size=4
+)
 
-trainer = DPOTrainer(model=model, args=training_args, processing_class=tokenizer, train_dataset=train_dataset)
-print("about to train!")
-trainer.train()
+trainer = PPOTrainer(config=config, model=model, tokenizer=tokenizer)
+
+prompts = []
+
+for prompt in prompts:
+    
+    response = ppo_trainer.generate(prompt)
+    reward = compute_reward(response)  # 
+    ppo_trainer.step([prompt], [response], [reward])
